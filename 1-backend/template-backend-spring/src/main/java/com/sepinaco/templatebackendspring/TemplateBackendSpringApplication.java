a961f120029307e05e697fbacc395bbb9d5237cd
espisepi <sepinaco@gmail.com>
2023-03-31 15:29:48 +0200

    spring hello world initializr - java 20, maven, any dependencies added

---
+++ 1-backend/template-backend-spring/src/main/java/com/sepinaco/templatebackendspring/TemplateBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sepinaco.templatebackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateBackendSpringApplication.class, args);
+	}
+
+}
---
+++ 1-backend/template-backend-spring/src/test/java/com/sepinaco/templatebackendspring/TemplateBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sepinaco.templatebackendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
