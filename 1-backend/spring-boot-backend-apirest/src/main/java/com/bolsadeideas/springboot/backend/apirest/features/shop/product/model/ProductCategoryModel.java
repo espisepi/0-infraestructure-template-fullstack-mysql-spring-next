49970f218bf99ee70c4a1ea174c138f79387115d
espisepi <sepinaco@gmail.com>
2023-04-16 17:37:12 +0200

    created BaseEntity and refactor ProductCategoryModel

--- 1-backend/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/features/shop/product/model/ProductCategoryModel.java
+++ 1-backend/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/features/shop/product/model/ProductCategoryModel.java
@@ -1,74 +1,34 @@
 package com.bolsadeideas.springboot.backend.apirest.features.shop.product.model;
 
-import java.io.Serializable;
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
+
+import com.bolsadeideas.springboot.backend.apirest.models.entity.BaseEntity;
 
 
 @Entity
 @Table(name="product_category")
-public class ProductCategoryModel implements Serializable {
+public class ProductCategoryModel extends BaseEntity {
 	
 	private static final long serialVersionUID = 1L;
-	
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column
-	private Long id;
-	@Column(name = "created_at")
-	private Date createdAt;
-	@Column(name = "updated_at")
-	private Date updatedAt;
 	
 	
 	@Column(unique=true, length=100)
 	private String name;
 	
 	
-	
+	public ProductCategoryModel() {
+		super();
+	}
+
+
 	// getters y setters generados con eclipse automaticamente ===========================
-
-	public Long getId() {
-		return id;
-	}
-
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-
-	public Date getCreatedAt() {
-		return createdAt;
-	}
-
-
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
-
-
-	public Date getUpdatedAt() {
-		return updatedAt;
-	}
-
-
-	public void setUpdatedAt(Date updatedAt) {
-		this.updatedAt = updatedAt;
-	}
-
 
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
---
+++ 1-backend/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/BaseEntity.java
@@ -0,0 +1,53 @@
+package com.bolsadeideas.springboot.backend.apirest.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column
+	private Long id;
+	@Column(name = "created_at")
+	private Date createdAt;
+	@Column(name = "updated_at")
+	private Date updatedAt;
+	
+	
+	public BaseEntity() {
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	
+	
+
+}
